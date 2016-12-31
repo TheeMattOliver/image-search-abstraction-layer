c525cdded269f32a338be21ae9cad025b83e068d
Matt Oliver <mattoliver.mattoliver@gmail.com>
2016-12-31 00:13:10 -0600

    front-end working, API call not working

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,26 @@
 angular.module('imgurApp', [])
- .controller('formCtrl', function($scope) {
-    $scope.master = {searchTerm:"Your search term..."};
-    $scope.fetch = function() {
-        $scope.user = angular.copy($scope.master);
-    };
-    $scope.fetch();
+
+// .service('item', function($http) {
+//   return $http({method: 'GET', url:'/search/:item'}).
+//          then(function(response) {
+//            console.log('This is the response.data', response.data);
+//            return response.data;
+//          });
+// })
+//  .controller('formCtrl', function($scope) {
+//     $scope.user = angular.copy($scope.master);
+// });
+
+
+angular.module("imgurApp", [])
+
+.controller("formCtrl", function($scope, $http) {
+  $http.get('/search/:item')
+    .success(function(data, status, headers, config) {
+      console.log("Success!")
+      $scope.results = data;
+    })
+    .error(function(data, status, headers, config) {
+      // log error
+    });
 });
-
-
-angular.module('MyApp', [])
-  .controller('MyCtrl', ['$scope', '$http', function ($scope, $http) {
-      $scope.user = {};
-      $scope.results = [];
-
-      $scope.search = function () {
-          /* the $http service allows you to make arbitrary ajax requests.
-           * in this case you might also consider using angular-resource and setting up a
-           * User $resource. */
-          $http.get('/your/url/search', { params: user },
-            function (response) { $scope.results = response; },
-            function (failure) { console.log("failed :(", failure); });
-      }
-  }]);
