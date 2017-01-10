415bf47b9eab772fefe46f6f140d9619bd000a15
Matt Oliver <mattoliver.mattoliver@gmail.com>
2017-01-09 22:58:11 -0600

    images not injecting yet

--- public/view3/view3.js
+++ public/view3/view3.js
@@ -9,6 +9,15 @@ angular.module('myApp.view3', ['ngRoute'])
   });
 }])
 
-.controller('View3Ctrl', [function() {
-
-}]);
+.controller('View3Ctrl', function($scope, $http) {
+      $http.get('/search/')
+        .success(function(data, status, headers, config) {
+          console.log('Success!');
+          console.log("Here's the data: ", data);
+          $scope.results = data;
+        })
+        .error(function(data, status, headers, config) {
+          // log error
+          console.log('Error')
+        });  
+  })
